a0f4efaa092865b70192ddbd80c3aeb1de05046b
deanm <34401253+dean2000@users.noreply.github.com>
2019-02-23 16:30:58 +0200

    Update list_applications.py

--- list_applications.py
+++ list_applications.py
@@ -5,7 +5,7 @@ date = datetime.datetime.now().strftime('%d-%m-%y')
 
 history_1 = open("/var/log/apt/history.log.1", 'r')
 history_2 = open("/var/log/apt/history.log", 'r')
-install = open((f'/home/dean/Documents/python_files/apps_installation/installation_file{date}.txt'),'w')
+install = open((f'/apps_installation/installation_file{date}.txt'),'w')
 
 finds = re.findall("apt install.*", (history_1.read()))
 finds += re.findall("apt install.*", (history_2.read()))
